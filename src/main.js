720d9c7849608443815753f72a84aee5f5c3b491
edvaldotorres <edvaldo.torres@tisaude.com>
2023-07-05 21:02:03 -0300

    fix: config axios fixeding

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import './plugins/axios'
 import { createApp } from 'vue'
-import App from './App.vue'
+import '@/plugins/axios'
 import router from './router'
 import store from './store'
+import App from '@/App.vue'
 
 createApp(App).use(store).use(router).mount('#app')
