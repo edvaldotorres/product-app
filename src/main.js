5b21493d2cbe76ef53ddd1454b7cbc56d93e5c2c
edvaldotorres <edvaldo.torres@tisaude.com>
2023-07-05 21:58:27 -0300

    feature: add tailwindcss

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import '@/plugins/axios'
 import router from './router'
 import store from './store'
 import App from '@/App.vue'
+import "@/assets/styles/main.css"
 
 createApp(App).use(store).use(router).mount('#app')
