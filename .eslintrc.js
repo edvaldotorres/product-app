9f429ed7b38ebd7c66b07bc3cd5883409c7029cb
edvaldotorres <edvaldo.torres@tisaude.com>
2023-07-07 20:10:40 -0300

    config: add configures the slint and babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,17 @@
 module.exports = {
-    root: true,
+    parserOptions: {
+        "parser": "babel-eslint",
+    },
     env: {
+        browser: true,
         node: true,
+        es6: true
     },
     extends: [
-        'plugin:vue/vue3-essential',
         'eslint:recommended',
+        'plugin:vue/vue3-recommended',
     ],
+    rules: {
+        'vue/multi-word-component-names': 0,
+    },
 }
