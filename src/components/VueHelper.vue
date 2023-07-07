d15c4a28d950ddb8787001d1bd7912440e0c99e5
edvaldotorres <edvaldo.torres@tisaude.com>
2023-07-07 20:12:48 -0300

    feature: add components and products and layouts project

---
+++ src/components/VueHelper.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="vue-helper">
+    <p>
+      Para obter um guia e receitas sobre como configurar/personalizar este projeto,
+      confira a 
+      <a
+        href="https://cli.vuejs.org"
+        target="_blank"
+        rel="noopener"
+      >documentação do vue-cli</a>.
+    </p>
+    <h3>Plug-ins CLI instalados</h3>
+    <ul>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+          target="_blank"
+          rel="noopener"
+        >babel</a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
+          target="_blank"
+          rel="noopener"
+        >eslint</a>
+      </li>
+    </ul>
+    <h3>Links Essenciais</h3>
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+          rel="noopener"
+        >Documentos principais</a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+          rel="noopener"
+        >Fórum</a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+          rel="noopener"
+        >Bate-papo da comunidade</a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+          rel="noopener"
+        >Twitter</a>
+      </li>
+      <li>
+        <a
+          href="https://news.vuejs.org"
+          target="_blank"
+          rel="noopener"
+        >Notícias</a>
+      </li>
+    </ul>
+    <h3>Ecossistema</h3>
+    <ul>
+      <li>
+        <a
+          href="https://router.vuejs.org"
+          target="_blank"
+          rel="noopener"
+        >vue-router</a>
+      </li>
+      <li>
+        <a
+          href="https://vuex.vuejs.org"
+          target="_blank"
+          rel="noopener"
+        >vuex</a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-devtools#vue-devtools"
+          target="_blank"
+          rel="noopener"
+        >vue-devtools</a>
+      </li>
+      <li>
+        <a
+          href="https://vue-loader.vuejs.org"
+          target="_blank"
+          rel="noopener"
+        >vue-loader</a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+          rel="noopener"
+        >awesome-vue</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    // eslint-disable-next-line vue/require-default-prop
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/products/list/ProductDetail.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="row border-1 p-2">
+    <div class="col-1 text-left">
+      {{ index + 1 }}
+    </div>
+    <div class="col-3">
+      {{ product.name }}
+    </div>
+    <div class="col-2">
+      <strong class="text-danger">R$ {{ product.price }}</strong>
+    </div>
+    <div class="col-3">
+      {{ product.description }}
+    </div>
+    <div class="col-2">
+      <router-link
+        :to="{ name: 'ProductEdit', params: { id: product.id } }"
+        class="btn btn-primary mr-2"
+        title="Edit Product"
+      >
+        <i class="fa fa-pencil" />
+      </router-link>
+      <button
+        class="btn btn-danger mx-2"
+        title="Delete Product"
+        @click="deleteProductModal(product.id)"
+      >
+        <i class="fa fa-trash" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "ProductDetail",
+  props: {
+    product: {
+      type: Object,
+      required: true,
+    },
+    index: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+
+  computed: { ...mapGetters(["isDeleting", "deletedData"]) },
+
+  methods: {
+    ...mapActions(["deleteProduct", "fetchAllProducts"]),
+    deleteProductModal(id) {
+      this.$swal
+        .fire({
+          text: "Você tem certeza que deseja deletar este produto?",
+          icon: "error",
+          cancelButtonText: "Não, Cancelar",
+          confirmButtonText: "Sim, Deletar",
+          showCancelButton: true,
+        })
+        .then((result) => {
+          if (result["isConfirmed"]) {
+            // Put delete logic
+            this.deleteProduct(id);
+            this.fetchAllProducts({
+              page: 1,
+              search: ''
+            });
+            this.$swal.fire({
+              text: "Produto deletado com sucesso!",
+              icon: "success",
+              position: 'top-end',
+              timer: 1000,
+            });
+          }
+        });
+    },
+  },
+};
+</script>
