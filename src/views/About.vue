2e147017b9bc83300b586839009c0c6f5e122533
edvaldotorres <edvaldo.torres@tisaude.com>
2023-07-08 14:24:22 -0300

    feature: add views

---
+++ src/views/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h4>
+      Sobre o Projeto
+    </h4>
+    <hr>
+    <div>
+      <p>
+        Este projeto foi desenvolvido com o intuito de aprender e praticar o Vue JS, Vuex e Vue Router.
+      </p>
+      <p>
+        <ul>
+          <li>Vue JS</li>
+          <li>Vuex Store Mangement</li>
+          <li>Aplicação CRUD completa</li>
+          <li>Sweet Alert</li>
+          <li>E muitos mais...</li>
+        </ul>
+      </p>
+    </div>
+  </div>
+</template>
